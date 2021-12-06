a04dc776c85109cb8e6cd1b8bb53bf717b2ccb9c
crashserver <crashserverlive@gmail.com>
2021-12-06 21:19:54 +0100

    add mverb and clouds in generator

--- FoxDot/server_generator/FoxDot/lib/Crashserver/crash_generator/server_conf.py
+++ FoxDot/server_generator/FoxDot/lib/Crashserver/crash_generator/server_conf.py
@@ -35,8 +35,8 @@ probChangeDegree = 3 # probability to change a synth degree, drum char or loop p
 probAddPlayerParam = 2 # probability to add a player param (.stutter, .spread, .shuffle)
 probAddPlayerAttribute = 4 # probability to a change a player attributes (dur, pan, sus)
 probAddEvent = 2 #probabilty to add a master event (change bpm, change root, master.lpf(), drop)
-probChangeAdsr = 3 # probabilty to change player adsr
-probAddFxOut = 1 # probability to add an fx output
+probChangeAdsr = 2 # probabilty to change player adsr
+probAddFxOut = 0 # probability to add an fx output
 
 #Add Master event Probability
 probChangeScale = 1

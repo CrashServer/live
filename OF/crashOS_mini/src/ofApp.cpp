3bbe172185240c895c57d217a3434348f8f47e1a
crashserver <crashserverlive@gmail.com>
2022-03-17 21:18:21 +0100

    up filter attack

--- OF/crashOS_mini/src/ofApp.cpp
+++ OF/crashOS_mini/src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    ofSetFrameRate(60);
+    ofSetFrameRate(5);
     // init variable
     //activeServer = false;
 
@@ -12,8 +12,14 @@ void ofApp::setup(){
 
     // OSC & UDP
     ofxUDPSettings settings;
+
+    ofxUDPManager manager;
+//    manager.SetNonBlocking("true");
+//    manager.
     settings.receiveOn(PORTUDP);
-    settings.blocking = false;
+
+    //settings.blocking = true;
+    udpConnection.SetNonBlocking(true);
     udpConnection.Setup(settings);
 }
 
@@ -21,7 +27,8 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 
-    // Osc & udp & arduino
+
+
     getData();
    }
 
@@ -45,27 +52,64 @@ void ofApp::getData(){
 //            }
 
     // Udp Code player *2 + Code Server
-    char udpMessage[500];
-    udpConnection.Receive(udpMessage,500);
-    string message=udpMessage;
-    char *msgType = &message[0];
-    if (message!=""){
-        if (*msgType == '#'){ // player 1
-            serial.writeByte('g'); // send arduino
-            cout << "SVDK : " + message.erase(0,1) << endl;
-            }
-        else if (*msgType == '!'){ // player 2
-            serial.writeByte('o'); // send Arduino
-            cout << "ZBDM : " + message.erase(0,1) << endl;
-            }
-        else if (*msgType == '@'){ // server code
-            serial.writeByte('r'); // send Arduino
-            cout << "SERVER : " + message.erase(0,1) << endl;
-            }
+    char udpMessage[5000];
+    string message;
+    string tempMessage;
+    bool getNext = true;
+
+    while(getNext){
+        udpConnection.Receive(udpMessage,5000);
+        tempMessage = udpMessage;
+        if (tempMessage==""){
+            getNext = false;
         }
-}
+        else {
+            message = tempMessage;
+        }
+    }
+
+        char *msgType = &message[0];
+        if (message!=""){
+            if (*msgType == '#'){ // player 1
+                serial.writeByte('g'); // send arduino
+                serial.flush();
+                cout << "\033[32mSVDK : \033[0m" + message.erase(0,1) << endl;
+                }
+            else if (*msgType == '!'){ // player 2
+                serial.writeByte('o'); // send Arduino
+                serial.flush();
+                cout << "\033[33mZBDM : \033[0m" + message.erase(0,1) << endl;
+                }
+            else if (*msgType == '@'){ // server code
+                serial.writeByte('r'); // send Arduino
+                serial.flush();
+                string randomServerCode = "\033[" + ofToString(int (ofRandom(1,100))) + "m";
+
+                cout << randomServerCode + "SERVER : " + message.erase(0,1) + "\033[0m"<< endl;
+                }
+              }
+        else {serial.flush();}
+    }
+
+
+//        if (message!=""){
+//            serial.writeByte('g'})
+//            if (*msgType == '#'){ // player 1
+//               serial.writeByte('g'); // send arduino
+//                }
+//            else if (*msgType == '!'){ // player 2
+//                serial.writeByte('o'); // send Arduino
+//                }
+//            else if (*msgType == '@'){ // server code
+//                serial.writeByte('r'); // send Arduino
+//                }
+//            }
+
+
+
 
 //-------------------------
+
 void ofApp::exit(){
 }
 

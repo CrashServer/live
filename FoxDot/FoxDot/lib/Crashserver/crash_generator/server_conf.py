3fca77fc9b20e21b87700aaf759f69daf8e57211
CrashServer <crashserverlive@gmail.com>
2023-06-09 19:56:07 +0200

    minor server conf, add fxout for server

--- FoxDot/FoxDot/lib/Crashserver/crash_generator/server_conf.py
+++ FoxDot/FoxDot/lib/Crashserver/crash_generator/server_conf.py
@@ -29,8 +29,8 @@ probChangeBpm = 2
 probAddLpf = 6
 probAddDrop = 3
 probAddKick = 4
-probAddAccompany = 5
-probAddFxOut = 0 # probability to add an fx output
+probAddAccompany = 3
+probAddFxOut = 1 # probability to add an fx output
 
 # Add player probability
 probAddSynth = 1

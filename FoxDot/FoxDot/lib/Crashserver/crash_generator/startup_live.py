a2f054e781048d5b5d7b5e9320c094053aa6501d
CrashServer <crashserverlive@gmail.com>
2024-02-21 16:52:55 +0100

    correct indent

--- FoxDot/FoxDot/lib/Crashserver/crash_generator/startup_live.py
+++ FoxDot/FoxDot/lib/Crashserver/crash_generator/startup_live.py
@@ -759,7 +759,7 @@ class MidiDwarf:
     def record(self, length=16, track=None):
         self.length=length
         if track is not None:
-			self.track(track)
+            self.track(track)
         self.recordStatus = "Start"
         Clock.schedule(self.send_record, Clock.mod(self.length))
         Clock.schedule(self.send_record, Clock.mod(self.length) + self.length)
@@ -772,12 +772,12 @@ class MidiDwarf:
     def stop(self, duration=0, track=None):
         if (duration != 0):
             Clock.schedule(self.send_start, Clock.mod(duration))
-			print(f"Stop {self.channelTrack} channel")
+            print(f"Stop {self.channelTrack} channel")
         else:
-			if track is not None:
-				self.track(track)
-				print(f"Stop {track} channel")
-            self.send_start()
+            if track is not None:
+                self.track(track)
+                print(f"Stop {track} channel")
+        self.send_start()
     def clear(self):
         self.send_control_change(3,0)
     def track(self, channelTrack=1):

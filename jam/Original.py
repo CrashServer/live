705b1f3df8e05ba20b1ce393c80b0bb494c804d0
Crash Server <47175530+CrashServer@users.noreply.github.com>
2024-08-31 08:53:32 +0200

    Update Original.py

--- jam/Original.py
+++ jam/Original.py
@@ -8,5 +8,6 @@ b4 >> bass(melody(),dur=var([2, 1, 1/4], [1, 2, 4]), drive=linvar([0, 0.1], 32),
 b5 >> bass(melody() + var([7, 3, [4, 0]]),dur=P[1/2, 2], drive=0, oct=4).unison(2).slider()
 b6 >> plaits(melody() + PWalk(8, 1, 1),dur=(1/2, P[1/2, 1/4, 1], 4, 2, 1/2, 1/2), engine=var([5, 7, 3], 8), drive=0, mverb=0.1, oct=4, amp=0.1, vol=0.5, pan=PWhite(linvar([-0.5, 0.5])))
 b7 >> plaits(melody(),dur=(1/2, 1/4), engine=3, drive=0, mverb=0.2, oct=5)
+Root.default = var([6, 2, 5])
 b8 >> bass(melody(),dur=1/4, drive=linvar([0, 0.1], 32), mverb=0.8, delay=var([0, 0.5]), oct=6)
 b9 >> bass(melody(),dur=var([1/4, 2],[13, 3]), drive=0, mverb=0).unison(2).every(13, "offmul", 2)

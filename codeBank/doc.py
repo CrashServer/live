a401093b1aec4dbe11e372fac4ad7ae52cefc8d8
crashserver <crashserver@gmail.com>
2024-10-29 18:27:16 +0100

    Refactor startup_live.py and Custom/startup.py
    - Refactor startup_live.py to improve code readability and add functionality to the pfonk function.
    - Refactor Custom/startup.py to fix indentation issues and add a reset function.
    
    Add codeBank/doc.py and codeBank/reset.py
    
    - Add doc.py to codeBank directory, which contains a list of useful functions and their documentation.
    - Add reset.py to codeBank directory, which contains functions to reset FoxDot.

---
+++ codeBank/doc.py
@@ -0,0 +1,25 @@
+# doc
+
+# list of cool custom functions
+pfonk() 
+# use: pfonk(PBin) -- without "" for doc
+# pfonk("lin") to find all functions with lin in name
+
+# list of short characters é,è,à,ê...
+pshort()
+
+# list of all samples
+psample()
+# use psample("kick") -- to search every kick sample
+
+# list of all synths
+psynth()
+# use psynth("fm") to see all attributes of the synth
+
+# list of all fx
+pfx()
+# use pfx("reverb") to see all attributes of the fx
+
+# list of all loops
+ploop()
+# use ploop("drum") -- to search every loop with drum in name

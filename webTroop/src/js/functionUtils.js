c3dbb7437b8d390bb8bdce7551c85c7393cec07a
crashserver <crashserver@gmail.com>
2024-12-24 16:40:09 +0100

    fix chat display, refacto main for module, fix stop in multi lines

---
+++ webTroop/src/js/functionUtils.js
@@ -0,0 +1,99 @@
+export const functionUtils = {
+    stopClock(wsServer) {
+        wsServer.send(JSON.stringify({
+            type: 'evaluate_code',
+            code: 'Clock.clear()\n'
+        }));
+    },
+
+    jumpToOtherPlayer(cm, awareness) {
+        const states = awareness.getStates();
+        states.forEach((state) => {
+          if (state.otherInstantCode) { 
+            const { user, code, position, line } = state.otherInstantCode;
+            if (user !== awareness.getLocalState().user.name){
+              console.log("jump to position: " + line + " " + position);
+              cm.setCursor({line: line-1, ch: position});
+            }
+          }
+        }
+    )},
+
+    resetChrono(wsServer) {
+        wsServer.send(JSON.stringify({
+          type: 'evaluate_code',
+          code: 'crashpanel.timeInit = time()\n'
+        }));
+    },
+
+    insertAttackContent(editor, attackContent) {
+        const cursor = editor.getCursor();
+        const line = cursor.line + 1; // Insérer en dessous de la ligne actuelle
+        editor.replaceRange(attackContent, { line: line, ch: 0 });
+    },
+
+    setMarker(cm, color="", txt="") {
+        const line = cm.getCursor().line
+        const info = cm.lineInfo(line);
+        if (info.handle.gutterClass != null && info.handle.gutterClass.includes(`line${color}`)){
+          cm.removeLineClass(line, "gutter")
+          cm.removeLineClass(line, "background")
+        }
+        else {
+          cm.removeLineClass(line, "gutter")
+          cm.removeLineClass(line, "background")
+          cm.addLineClass(line, "gutter", `line${color}`)
+          cm.addLineClass(line, "background", `line${color}`)
+          return true;
+          
+        }
+    },
+
+    ifPlayerStop(codeToEvaluate) {
+        const playerPattern = /^_([a-zA-Z]\d+|[a-zA-Z]{2})/;
+        const match = codeToEvaluate.trim().match(playerPattern);
+
+        if (match) {
+          const player = match[1];
+          return `${player}.stop()`;
+        }
+        return codeToEvaluate;
+    },
+
+    getCodeAndCheckStop(cm, multi=false) {
+        const cursor = cm.getCursor();
+        let startLine = cursor.line;
+        let endLine = cursor.line;
+
+        // Si multi-lignes
+        if (multi) {
+            // Recherche début du bloc (vers le haut)
+            while (startLine > 0 && cm.getLine(startLine - 1).trim() !== '') {
+                startLine--;
+            }
+
+            // Recherche fin du bloc (vers le bas)
+            while (endLine < cm.lineCount() - 1 && cm.getLine(endLine + 1).trim() !== '') {
+                endLine++;
+            }
+        }
+
+        // Extraire le bloc
+        const blockCode = cm.getRange(
+            {line: startLine, ch: 0},
+            {line: endLine, ch: cm.getLine(endLine).length}
+        );
+
+        if (blockCode.trim()) {
+            // Verifier s'il faut stopper un player
+            let blockCodeArray = blockCode.split('\n');
+            blockCodeArray.forEach((code, index) => {
+            blockCodeArray[index] = functionUtils.ifPlayerStop(code);
+            });
+            const blockCodeJoin = blockCodeArray.join('\n');
+            console.log(blockCodeJoin);
+            return [blockCodeJoin, startLine, endLine];
+        }
+        return [blockCode, startLine, endLine];
+    },
+};
--- webTroop/src/js/logs.js
+++ webTroop/src/js/logs.js
@@ -1,28 +1,56 @@
-// Redimensionner le panneau de logs
-  const separator = document.getElementById('separator')
-  const logs = document.getElementById('logs')
-  const editorContainer = document.getElementById('editor-container')
+export const logsUtils = {
+  isResizing: false,
+  editorContainer: document.getElementById('editor-container'),
+  logPanel: document.getElementById('logPanel'),
+  otherDisplay: document.getElementById('other-user-display'),
+  logs: document.getElementById('logs'),
 
-  let isResizing = false
+  initResize: function(editor) {
+    this.otherDisplay.addEventListener('mousedown', (e) => {
+      this.isResizing = true;
+      document.addEventListener('mousemove', this.resize.bind(this, editor));
+      document.addEventListener('mouseup', this.stopResize.bind(this));
+    });
+  },
 
-  separator.addEventListener('mousedown', (e) => {
-    isResizing = true
-    document.addEventListener('mousemove', resize)
-    document.addEventListener('mouseup', stopResize)
-  })
-
-  function resize(e) {
-    if (isResizing) {
-      const containerHeight = editorContainer.clientHeight
-      const newLogsHeight = containerHeight - e.clientY
-      logs.style.height = `${newLogsHeight}px`
-      editor.getWrapperElement().style.height = `${containerHeight - newLogsHeight - separator.clientHeight}px`
-      editor.refresh()
+  // Redimensionnement de la console
+  resize: function(editor, e) {
+    if (this.isResizing) {
+      const containerHeight = this.editorContainer.clientHeight;
+      const newLogsHeight = containerHeight - e.clientY;
+      this.logPanel.style.height = `${newLogsHeight}px`;
+      editor.getWrapperElement().style.height = `${containerHeight - newLogsHeight}px`;
+      editor.refresh();
     }
-  }
+  },
 
-  function stopResize() {
-    isResizing = false
-    document.removeEventListener('mousemove', resize)
-    document.removeEventListener('mouseup', stopResize)
+  // Arrêt du redimensionnement
+  stopResize: function() {
+    this.isResizing = false;
+    document.removeEventListener('mousemove', this.resize);
+    document.removeEventListener('mouseup', this.stopResize);
+  },
+
+  // Affichage du code de l'autre joueur
+  insertOtherUserCode: function(line, code) {
+    this.otherDisplay.innerHTML = `${line}: ${code}`;
+  },
+
+  // Ajout des logs dans la console
+  appendLog(message, color) {
+    const entry = document.createElement('pre');
+    entry.className = 'log-entry';
+    if (color){
+      entry.style.color = color;
+    }
+    if (message.includes('Traceback')) {
+      entry.classList.add('error-log');
+    }
+    else if (!message.includes('>>')) {
+      entry.classList.add('help');
+    }
+    entry.textContent = message;
+    this.logs.insertBefore(entry, logs.firstChild);
+    this.logs.scrollTop = 0;
   }
+};

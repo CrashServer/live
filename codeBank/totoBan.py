445051ae0804955ea54b84db199fff00d8a26750
crashserver <crashserver@gmail.com>
2025-03-14 16:31:51 +0100

    codebank: add totoban et nuance feat: variation add help

---
+++ codeBank/totoBan.py
@@ -0,0 +1,10 @@
+# totoBan
+Clock.bpm=173
+f8 >> loop("beats8", dur=8, sample=5)
+c1 >> loop("berlin8", dur=8, sample=4)
+m0 >> loop("cinambi8", dur=8, sample=5)
+v3 >> loop("circlebreak16", dur=16, sample=7, comp=1, sbrk=.5, sbrkdur=.5)
+b4 >> loop("nbvarp16", dur=32, sample=5, hpf=120)
+
+x4 >> loop("ragedrum16", dur=32, sample=5, amp=1, comp=1, fx=1)
+r9 >> loop("electrodrum16", dur=16, sample=3, comp=1)

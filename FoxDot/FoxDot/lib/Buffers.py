213a44eee095c94b988eea1931cabf49afb6202e
crashserver <crashserver@gmail.com>
2025-04-12 01:04:17 +0200

    + add lfo for samples / loop synth + remove lfo from autocomplete + add lfoadd & lfomul

--- FoxDot/FoxDot/lib/Buffers.py
+++ FoxDot/FoxDot/lib/Buffers.py
@@ -483,6 +483,7 @@ class LoopSynthDef(SampleSynthDef):
         self.defaults['looping'] = 1.0
         self.defaults['clip'] = 0
         self.base.append("rate = (rate * (1-(beat_stretch>0))) + ((BufDur.kr(buf) / sus) * (beat_stretch>0));")
+        self.base.append("rate = if(ratelfo<=0, rate, rate * SinOsc.kr(ratelfo, 0, ratelfomul, ratelfoadd));")
         self.base.append("clip = if(clip<=0, 1, (clip/sus)*0.5);")
         self.base.append("osc = PlayBuf.ar(2, buf, BufRateScale.kr(buf) * rate, startPos: BufSampleRate.kr(buf) * pos, loop: looping);")
         self.base.append("osc = osc * EnvGen.ar(Env([0,1,1,0],[0.05, (sus*clip)-0.05, 0.05]));")

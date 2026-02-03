df303c80cf4819c1ed94822108e0b12dc818f0f1
Crash Server <47175530+CrashServer@users.noreply.github.com>
2026-02-03 10:09:30 +0100

    Add alvaofcpudeath.py with initial clock and settings

---
+++ codeBank/alvaofcpudeath.py
@@ -0,0 +1,4 @@
+# alvaofdeath 96
+# todo
+Clock.bpm = 96
+m2 >> alva(dur=PDur([5, 3, 7, 5, 9, 5, 6], 12), sus=0.76, fbdelay=0.1, gate=0.1, gaterate=1, gatewave=1, leg=PRand(8)[:13], glitch=0.1, oct=(3, 4), tube=1,fshift=var([0, 1, 2, 4, 0], PRand([1,2,4,8])), shimmer=PWhite(0, 0.2)[:8], shimsize=0.2, shimpitch=var([1, 1.5, 2], PRand(32)), shimmix=1, tape=0, fbfeed=0.9, fbtime=0.2, tapedrive=4, drift=32, envdist=0, lpf=4000, lpr=0.2, mu=4) #tapedrive #dirtybass #mu 

8f9af4c278ab9c4cb9afa86987b7caa57ca718c4
Crash Server <47175530+CrashServer@users.noreply.github.com>
2026-03-26 07:35:32 +0100

    Create pisspattern.py

---
+++ codeBank/pisspattern.py
@@ -0,0 +1,88 @@
+# pisspattern #documentation
+# 170
+
+
+#@intro(64)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.5, sblur=0.0, sample=11, sbluramt=0.3, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5).unison(3)
+
+#@build(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.5, sblur=0.0, sample=11, sbluramt=0.3, csweep=0.2, cswfreq=70, cswdepth=0.2, cswrate=0.5, cswdecay=0.5).unison(3)
+
+#@peak(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.5, sblur=0.1, sbluramt=1, csweep=0.1, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@break(32)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.5, sblur=0.1, sbluramt=2, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@drop(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.1, sbluramt=2, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+v1 >> play("x ")
+
+#@outro(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.1, sbluramt=2, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part7(16)
+v1 >> play("X ")
+
+#@part8(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.1, sbluramt=1, csweep=0.1, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part9(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.1, sbluramt=2, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part10(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.1, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part11(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.4, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.0, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part12(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.3, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=0.5, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part13(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0.3, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part14(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=1, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=0.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part15(4)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=4, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part16(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part17(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1.5, spwarp=0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part18(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1.5, spwarp=0.4, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part19(8)
+v1 >> play("X ", amp=4)
+v1 >> play("X ", amp=4)
+
+#@part20(16)
+v1 >> play("X ", amp=12)
+
+#@part21(4)
+v1 >> play("X ", amp=12, sample=4)
+
+#@part22(4)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1.5, spwarp=0.4, spwstr=2, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part23(4)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1.5, spwarp=0.4, spwstr=0, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part24(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1.5, spwarp=0.4, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=1.5, spwarp=0.0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part25(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=0, spwarp=0.0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.7).unison(3)
+
+#@part26(8)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=0, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=0, spwarp=0.0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.0).unison(3)
+
+#@part27(16)
+~d1 >> play(pbuild("techno", 16, fill=8), dur=0.25, sblur=1, sbluramt=4, csweep=0.2, cswfreq=70, cswdepth=0.5, cswrate=4, cswdecay=0, spwarp=0.0, spwstr=1, spwshift=1, gdel=0.1, gdeltime=0.01, gdelsize=0.01, gdelsprd=0.8, gdelfb=0.0).unison(3)

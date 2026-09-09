cffb7519e9d9a87ffec6cfc68975a08d2ed9ac55
crashserver <crashserverlive@gmail.com>
2026-09-09 19:24:15 +0200

    suprression de codebank pour remplacement par sous-module

--- codeBank/C1.py
+++
@@ -1,6 +0,0 @@
-# C1
-c1 >> loop("core16", dur=16, sample=1, cut=1, lpf=0)
-c2 >> loop("xvermin16", dur=16, sample=2).unison(2)
-c3 >> loop("xbassphase16", dur=16, mverb=0.3, sample=2).unison(2)
-c5 >> loop("electrodrum32", dur=32, sample=0, lpf=0)
-c6 >> loop("break32", dur=32, sample=2, lpf=0)
